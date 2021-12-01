bd3b89929c8393b6ac13ab77ac93353f57f95095
zg <52820470+zguo0525@users.noreply.github.com>
2021-12-01 17:14:09 -0500

    Update model.py

--- model.py
+++ model.py
@@ -230,3 +230,164 @@ class DBlock3D(layers.Layer):
             fx = dsample3d(fx)
             x = dsample3d(x)
         return x + fx
+    
+class AxialAttention(layers.Layer):
+    """spectral norm axial attention layer. 
+    """
+    def __init__(self, att_axis, groups, in_planes, out_planes, kernel_size, att_name, strides = 1):
+        super(AxialAttention, self).__init__()
+        
+        self.att_axis = att_axis
+        self.groups = groups
+        self.in_planes = in_planes
+        self.out_planes = out_planes
+        self.group_planes = out_planes // groups
+        self.kernel_size = kernel_size
+        self.strides = strides
+        
+        self.bn_qkv = BatchNormalization()
+        self.bn_similarity = BatchNormalization()
+        self.bn_output = BatchNormalization()
+        
+        self.qkv_transform = tfa.layers.SpectralNormalization(Conv1D(filters = 2 * out_planes, kernel_size = 1, strides = 1, padding = 'same', use_bias = False, data_format = "channels_first",
+                                    kernel_initializer = tf.keras.initializers.RandomNormal(0.0, tf.math.sqrt(1.0 / self.in_planes))))
+        
+        self.relative = tf.Variable(tf.random.normal(shape = (self.group_planes * 2, kernel_size * 2 - 1), mean = 0.0, stddev = tf.math.sqrt(1.0 / self.group_planes)), 
+                                    trainable = True, name = 'rel_' + att_name)
+        query_index = tf.expand_dims(tf.range(kernel_size), axis = 0)
+        key_index = tf.expand_dims(tf.range(kernel_size), axis = 1)
+        relative_index = key_index - query_index + kernel_size - 1
+        self.flatten_index = tf.reshape(relative_index, -1)
+        
+        if self.strides > 1:
+            self.pooling = AveragePooling3D(pool_size = (strides, strides, strides), strides = strides)
+            
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'att_axis': self.att_axis,
+            'kernel': self.kernel_size,
+            'groups': self.groups,
+            'in_planes': self.in_planes,
+            'out_planes': self.out_planes,
+        })
+        return config
+            
+    def call(self, x):
+        # x: B x H x W x L x C
+        if self.att_axis == 'H':
+            x = tf.transpose(x, [0, 2, 3, 4, 1])
+            
+        elif self.att_axis == 'W':
+            x = tf.transpose(x, [0, 1, 3, 4, 2])
+            
+        elif self.att_axis == 'L':
+            x = tf.transpose(x, [0, 1, 2, 4, 3])
+            
+        # Let self.att_axis == 'H', then x: B x W x L x C x H
+        B, W, L, C, H = x.shape
+        x = tf.reshape(x, [B * W * L, C, H])
+        
+        qkv = self.bn_qkv(self.qkv_transform(x))
+        q, k, v = tf.split(tf.reshape(qkv, [B * W * L, self.groups, self.group_planes * 2, H]), 
+                           [self.group_planes // 2, self.group_planes // 2, self.group_planes], axis = -2)
+    
+        all_embeddings = tf.reshape(tf.gather(self.relative, self.flatten_index, axis = 1), [self.group_planes * 2, self.kernel_size, self.kernel_size])
+        q_embedding, k_embedding, v_embedding = tf.split(all_embeddings, [self.group_planes // 2, self.group_planes // 2, self.group_planes], axis = 0)
+        
+        # q: BWL x Nh x dqh x H, q_embedding: dqh x H x H -> qr: BWL x Nh x H x H.
+        qr = tf.einsum('bgci,cij -> bgij', q, q_embedding)
+        kr = tf.transpose(tf.einsum('bgci,cij -> bgij', k, k_embedding), [0, 1, 3, 2])
+        qk = tf.einsum('bgci, bgcj -> bgij', qr, kr)
+        
+        stacked_similarity = tf.concat([qk, qr, kr], axis = 1)
+        stacked_similarity = tf.math.reduce_sum(tf.reshape(self.bn_similarity(stacked_similarity), [B * W * L, 3, self.groups, H, H]), axis = 1)
+        
+        similarity = tf.nn.softmax(stacked_similarity, axis = 3)
+        sv = tf.einsum('bgij,bgcj -> bgci', similarity, v)
+        sve = tf.einsum('bgij,cij -> bgci', similarity, v_embedding)
+        stacked_output = tf.reshape(tf.concat([sv, sve], axis = -1), [B * W * L, self.out_planes * 2, H])
+        output = tf.math.reduce_sum(tf.reshape(self.bn_output(stacked_output), [B, W, L, self.out_planes, 2, H]), axis = -2)
+        
+        if self.att_axis == 'H':
+            output = tf.transpose(output, [0, 4, 1, 2, 3])
+            
+        elif self.att_axis == 'W':
+            output = tf.transpose(output, [0, 1, 4, 2, 3])
+            
+        elif self.att_axis == 'L':
+            output = tf.transpose(output, [0, 1, 2, 4, 3])
+        
+        if self.strides > 1:
+            output = self.pooling(output)
+            
+        return output
+    
+class DBlock3D_att(layers.Layer):
+    """downsampling blcok with spectral norm axial attention layer. 
+    """
+    def __init__(self,
+                 filters,
+                 downsample=True,
+                 name='block',
+                 groups=4):
+        super(DBlock3D_att, self).__init__(name=name)
+        self.filters = filters
+        self.downsample = downsample
+        self.groups = groups
+
+        self.conv1 = SNConv3d(filters, (1, 1, 1), (1, 1, 1), use_bias=False, name='snconv_1')
+        self.conv2 = SNConv3d(2*filters, (1, 1, 1), (2, 2, 1), use_bias=False, name='snconv_2')
+        self.conv3 = SNConv3d(2*filters, (1, 1, 1), (2, 2, 1), use_bias=False, name='snconv_3')
+        
+        self.bn1 = BatchNormalization()
+        self.bn2 = BatchNormalization()
+        self.bn3 = BatchNormalization()
+        
+    def build(self, input_shape):
+        self.height_block = AxialAttention(att_axis = 'H', groups = self.groups, in_planes = self.filters, out_planes = self.filters, kernel_size = int(input_shape[-4]), att_name = 'block' + '_h')
+        self.width_block = AxialAttention(att_axis = 'W', groups = self.groups, in_planes = self.filters, out_planes = self.filters, kernel_size = int(input_shape[-3]), att_name = 'block' + '_w')
+        self.layer_block = AxialAttention(att_axis = 'L', groups = self.groups, in_planes = self.filters, out_planes = self.filters, kernel_size = int(input_shape[-2]), att_name = 'block' + '_l',
+                                          strides = 1)
+    
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'filters': self.filters,
+            'downsample': self.downsample,
+            'groups': self.groups,
+        })
+        return config
+
+    def call(self, x, training=None):
+        
+        fx = self.conv1(x, training=training)
+        
+        fx = tf.nn.relu(self.bn1(fx))
+        
+        fx = self.height_block(fx)
+        fx = self.width_block(fx)
+        fx = self.layer_block(fx)
+        fx = tf.nn.relu(fx)
+        
+        fx = self.conv2(fx, training=training)
+        fx = self.bn2(fx)
+        
+        if self.downsample:
+            x = self.conv3(x, training=training)
+            x = self.bn3(x)
+            
+        return tf.nn.relu(x + fx)
+    
+    
+def d_hinge_loss(logits_real, logits_fake):
+    "hinge loss for discriminator
+    "
+    loss_real = tf.nn.relu(1.0 - logits_real)
+    loss_fake = tf.nn.relu(1.0 + logits_fake)
+    return (tf.reduce_mean(loss_real) + tf.reduce_mean(loss_fake)) * (1.0 / batch) 
+
+def g_hinge_loss(logits_fake):
+    "hinge loss for generator
+    "
+    return -tf.reduce_mean(logits_fake) * (1.0 / batch)
